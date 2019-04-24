6ffd814bdb69618d6177e70f3800e91340479f80
liuchunyueh <liuchunyueh@gmail.com>
2019-04-24 21:57:52 +0800

    set update action for admin to change role of user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,4 +9,21 @@ class Admin::UsersController < ApplicationController
       @user = User.find(params[:id])
     end
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_users_path
+      flash[:notice] = "Change role status successfully!"
+    else
+      @category = Category.all
+      render :index  
+    end  
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:role)
+  end
 end
