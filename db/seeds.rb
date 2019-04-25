fe6a7694d3efd8b798716010f507878ef2c02257
liuchunyueh <liuchunyueh@gmail.com>
2019-04-25 17:00:39 +0800

    add seed data of category

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,24 @@
 
 User.create(email: "admin@example.com", password: "12345678", role: "admin")
 puts "Default admin created!"
+
+#add default category
+
+Category.destroy_all
+
+category_list = [
+  { name: "貓版" },
+  { name: "狗版" },
+  { name: "食記" },
+  { name: "體育" },
+  { name: "3C" },
+  { name: "心情" },
+  { name: "男女" }
+]
+
+
+category_list.each do |category|
+  Category.create( name: category[:name])
+end  
+
+puts "category created!!"
